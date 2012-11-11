c0e9fc65cf5b49dba17ea9e06126ea3ab30dce0e
trungvdt49 <trungvdt49@22f3fac7-047e-1a53-cc77-ba8e544d8870>
2012-11-11 06:46:52 +0000

    Add MoodleCategory HocKy

---
+++ trunk/CongThongTinSV/Models/MoodleCategoryModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CongThongTinSV.Models
+{
+    public class MoodleCreateCategoryResponse
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class MoodleHocKy
+    {
+        [DisplayName("ID")]
+        public int ID { get; set; }
+
+        [DisplayName("ID moodle")]
+        public int ID_moodle { get; set; }
+
+        [DisplayName("Đợt")]
+        public int Dot { get; set; }
+
+        [DisplayName("Học kỳ")]
+        public int Hoc_ky { get; set; }
+
+        [DisplayName("Năm học")]
+        public string Nam_hoc { get; set; }
+    }
+}
