4639665666402a285e915d26cb06d4eb39755a11
trungvdt49 <trungvdt49@22f3fac7-047e-1a53-cc77-ba8e544d8870>
2012-11-13 15:04:05 +0000

    Update MoodleCourse LopTinChi, UtilityController, _Layout.cshtml, _LoginPartial.cshtml, add MoodleLopTinChiModel

---
+++ trunk/CongThongTinSV/Models/MoodleLopTinChiModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CongThongTinSV.Models
+{
+    public class MoodleCreateCourseResponse
+    {
+        public string id { get; set; }
+        public string shortname { get; set; }
+    }
+
+    public class MoodleLopTinChi
+    {
+        [DisplayName("ID")]
+        public int ID_lop_tc { get; set; }
+
+        [DisplayName("ID moodle")]
+        public int ID_moodle { get; set; }
+
+        [DisplayName("Lớp học phần")]
+        public string Lop_hoc_phan { get; set; }
+
+        [DisplayName("Ký hiệu")]
+        public string Ky_hieu { get; set; }
+
+        [DisplayName("Từ ngày")]
+        public DateTime Tu_ngay { get; set; }
+
+        [DisplayName("Đến ngày")]
+        public DateTime Den_ngay { get; set; }
+    }
+}
